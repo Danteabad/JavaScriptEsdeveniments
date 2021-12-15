8fced635e2ff72ff20095f2803ff718400525fcd
Danteabad <dante.abad@institutvidreres.cat>
2021-12-15 11:50:34 +0100

    EX3

--- EJ_5.js
+++ EJ_5.js
@@ -1,7 +1,18 @@
 //EX2
-document.onkeydown=function(letter){
+/*document.onkeydown=function(letter){
     document.getElementById('teclat').innerHTML=letter.key;
 }
+document.onkeyup=function(){
+    document.getElementById('teclat').innerHTML=' ';
+}*/
+
+//EX3
+document.onkeydown=function(letter){
+    document.getElementById('teclat').innerHTML=letter.key;
+    var contador = document.getElementById('acumula');
+    contador.innerHTML += letter.key + ' '
+}
+
 document.onkeyup=function(){
     document.getElementById('teclat').innerHTML=' ';
 }
